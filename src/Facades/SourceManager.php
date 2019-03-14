71fbc9d12a08307012eb5915a2bf128267d7d0d4
Semyon Chetvertnyh <semyon.chetvertnyh@gmail.com>
2019-03-14 14:45:35 +0200

    Add PHPDoc to Facade

--- src/Facades/SourceManager.php
+++ src/Facades/SourceManager.php
@@ -5,6 +5,14 @@ namespace Torann\Currency\Facades;
 use Illuminate\Support\Facades\Facade;
 use Torann\Currency\SourceManager as Factory;
 
+/**
+ * @method static update()
+ * @method static \Torann\Currency\Contracts\SourceInterface source($name = null)
+ * @method static \Torann\Currency\Contracts\SourceInterface driver($name = null)
+ * @method static string getDefaultDriver()
+ * @method static string fetchesFrom()
+ * @method static static fetchFrom($source)
+ */
 class SourceManager extends Facade
 {
     /**
