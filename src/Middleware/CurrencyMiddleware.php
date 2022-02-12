639d454ad7aa82044aacb8804f03315c906810c5
Christopher C. Okonkwo <45552824+christopherokonkwo@users.noreply.github.com>
2022-02-12 21:27:13 +0100

    Update CurrencyMiddleware.php

--- src/Middleware/CurrencyMiddleware.php
+++ src/Middleware/CurrencyMiddleware.php
@@ -49,7 +49,7 @@ class CurrencyMiddleware
         }
 
         // Get currency from session
-        $currency = $request->getSession()->get('currency');
+        $currency = $request->session()->get('currency');
         if ($currency && currency()->isActive($currency) === true) {
             return $currency;
         }
